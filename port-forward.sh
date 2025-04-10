a0aca95091aee9427a99c4f57467d3f9b2f33754
eikarna <nixic0@proton.me>
2025-04-10 09:28:31 +0700

    upload new files

--- port-forward.sh
+++ port-forward.sh
@@ -2,5 +2,14 @@
 # Pastikan IP forwarding diaktifkan
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
-# Tambahkan aturan iptables untuk melakukan port forwarding UDP ke port 50000
-iptables -t nat -A PREROUTING -p udp --dport 1:65535 -j REDIRECT --to-port 50000
+interface=$(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1)
+
+# UDP CUSTOM: Tambahkan aturan iptables untuk melakukan port forwarding UDP ke port 3671
+iptables -t nat -A PREROUTING -i ${interface} -p udp --dport 1:7299 -j DNAT --to-destination :3671
+
+# ZIVPN: Tambahkan aturan iptables untuk melakukan port forwarding UDP ke port 5667
+iptables -t nat -A PREROUTING -i ${interface} -p udp --dport 6000:19999 -j DNAT --to-destination :5667
+
+# Tambahan untuk ZIVPN Server
+ufw allow 6000:19999/udp
+ufw allow 5667/udp
