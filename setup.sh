cd73fc9ce242767eaa5eb50bd0143e5564f8a116
eikarna <nixic0@proton.me>
2025-04-17 18:28:34 +0700

    update: setup.sh

--- setup.sh
+++ setup.sh
@@ -7,5 +7,5 @@ ln -sf *.service /etc/systemd/system
 echo "Reloading systemctl daemon.."
 systemctl daemon-reload
 echo "Enabling service.."
-systemctl enable --now $(find $(pwd) -type f -name "*.service" -printf "%f\n")
+systemctl enable --now $(find $(pwd) -type f -name "*.service" -print0 | xargs -0 -n1 -P8 basename)
 # reboot
