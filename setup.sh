107b3a7d5c2409f97ae0c22fb7f95faab4fa8356
eikarna <nixic0@proton.me>
2025-04-17 18:41:25 +0700

    update: setup.sh

--- setup.sh
+++ setup.sh
@@ -2,10 +2,12 @@
 
 set -euo pipefail
 
+currentDir=`pwd`
+
 echo "Force linking all *.service files to /etc/systemd/system"
-ln -sf *.service /etc/systemd/system
+ln -sf $currentDir/*/**.service /etc/systemd/system
 echo "Reloading systemctl daemon.."
 systemctl daemon-reload
 echo "Enabling service.."
-systemctl enable --now $(find $(pwd) -type f -name "*.service" -print0 | xargs -0 -n1 -P8 basename)
+systemctl enable --now $(find $currentDir -type f -name "*.service" -print0 | xargs -0 -n1 -P8 basename)
 # reboot
